49a5ee14659d9902b392c0cd2604e7e5bde74bcd
Keaobaka Matlhola <111046000+kcmatlhola92@users.noreply.github.com>
2023-04-10 08:25:26 +0200

    My sql test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,5 @@
--- prepares a MySQL serever for the project
--- creates database
+-- Prepares a MySQL Test server for the project.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
--- create a user with password
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
--- grant all privileges.
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
--- grant select privileges.
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
